db5f8c481e8b86cfe79f90b7376a92021d60505e
alu10216804 <alubar804@ieselcaminas.org>
2021-09-30 20:27:07 +0200

    hasta el 41

--- app/src/main/java/com/example/intento2/MainActivity.kt
+++ app/src/main/java/com/example/intento2/MainActivity.kt
@@ -14,34 +14,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        enunciado.text = "Proyecto 30"
+        enunciado.text = "Proyecto 39"
 //        in1.hint="Ingrese dia"
 //        in2.hint="Ingrese mes"
 //        in3.hint="Ingrese año"
-        in1.isVisible= true
-        in2.isVisible= true
-        in3.isVisible= true
+        in1.isVisible= false
+        in2.isVisible= false
+        in3.isVisible= false
         in4.isVisible= false
         in5.isVisible= false
         button.setOnClickListener {
-            val num1 = in1.text.toString().toInt()
-            val num2 = in2.text.toString().toInt()
-            val num3 = in3.text.toString().toInt()
+//            val num1 = in1.text.toString().toInt()
+//            val num2 = in2.text.toString().toInt()
+//            val num3 = in3.text.toString().toInt()
 //            val num4 = in4.text.toString().toInt()
-//            val num5 = in5.text.toString().toInt()
-            //            val suma = num1 + num2 + num3
-//            val resta = num1 - num2
-//            val producto = num1 * num2
-//            val division = num1 / num2
-//          val cuadrado=num1*4
-//            val promedio = suma / 3
-//            val digitos = if (num1 < 10) 1  else 2
-//            val mensaje = if (promedio>=7) "Promocionado"
-//            else if (promedio>=4 && promedio<7) "Regular"
-//            else "Reprobado"
-            val menor = if (num1 < num2 && num1 < num3) num1 else if (num2 < num3) num2 else num3
-            val mayor = if (num1 > num2 && num1 > num3) num1 else if (num2 > num3) num2 else num3
-            resultado.text=("El mayor de la lista es $mayor y el menor $menor")
+//            //            val num5 = in5.text.toString().toInt()
+//            val arrayListo =  arrayOf<Int>(num1,num2,num3)
+            //            val resta = num1 - num2
+            //            val producto = num1 * num2
+            //            val division = num1 / num2
+            //          val cuadrado=num1*4
+            //            val promedio = suma / 3
+            var palabras=""
+
+            var mult8 = 8
+            while (mult8 <= 500) {
+                palabras+=("$mult8 -\n")
+                mult8 = mult8 + 8
+            }
+            resultado.text=("$palabras")
 
 }
     }
@@ -49,3 +50,16 @@ class MainActivity : AppCompatActivity() {
 
 }
 
+
+
+
+
+
+
+fun main(parametro: Array<String>) {
+    var mult8 = 8
+    while (mult8 <= 500) {
+        print("$mult8 -")
+        mult8 = mult8 + 8
+    }
+}
