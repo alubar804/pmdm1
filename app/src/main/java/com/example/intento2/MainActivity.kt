bbd26dd57381968c301403721fc98fdd28edde18
alu10216804 <alubar804@ieselcaminas.org>
2021-09-30 20:37:56 +0200

    hasta el 42

--- app/src/main/java/com/example/intento2/MainActivity.kt
+++ app/src/main/java/com/example/intento2/MainActivity.kt
@@ -14,35 +14,46 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        enunciado.text = "Proyecto 39"
+        enunciado.text = "Proyecto 42"
 //        in1.hint="Ingrese dia"
 //        in2.hint="Ingrese mes"
 //        in3.hint="Ingrese año"
-        in1.isVisible= false
+//        in1.isVisible= false
         in2.isVisible= false
         in3.isVisible= false
         in4.isVisible= false
         in5.isVisible= false
         button.setOnClickListener {
-//            val num1 = in1.text.toString().toInt()
+            val num1 = in1.text.toString().toInt()
 //            val num2 = in2.text.toString().toInt()
 //            val num3 = in3.text.toString().toInt()
 //            val num4 = in4.text.toString().toInt()
 //            //            val num5 = in5.text.toString().toInt()
-//            val arrayListo =  arrayOf<Int>(num1,num2,num3)
+//            val arrayListo =  arrayOf<Int>(num1,num2,num3,num4)
             //            val resta = num1 - num2
             //            val producto = num1 * num2
             //            val division = num1 / num2
             //          val cuadrado=num1*4
             //            val promedio = suma / 3
-            var palabras=""
+            var x =0
+            do {
+                //esto crea un bucle infinito y peta, no es traducible parece
+                x = num1
+                if (x>-1)
+                    if (x <10)
+                        resultado.text =("el numero $x tiene 1 cifra")
+                    else if (x <100)
+                        resultado.text =("el numero $x tiene 2 cifras")
+                    else if (x<1000)
+                        resultado.text =("el numero $x tiene 3 cifras")
+                    else
+                        resultado.text = "error"
+                else
+                    resultado.text = "negativo no validos"
+            }while (x !=0)
 
-            var mult8 = 8
-            while (mult8 <= 500) {
-                palabras+=("$mult8 -\n")
-                mult8 = mult8 + 8
-            }
-            resultado.text=("$palabras")
+
+
 
 }
     }
