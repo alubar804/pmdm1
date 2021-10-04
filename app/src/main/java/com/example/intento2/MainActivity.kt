34c7f607d963fe51ef3130b82315a5cc2b445a8b
alu10216804 <alubar804@ieselcaminas.org>
2021-10-04 19:05:41 +0200

    hasta el 47

--- app/src/main/java/com/example/intento2/MainActivity.kt
+++ app/src/main/java/com/example/intento2/MainActivity.kt
@@ -14,17 +14,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        enunciado.text = "Proyecto 42"
+        enunciado.text = "Proyecto 47"
 //        in1.hint="Ingrese dia"
 //        in2.hint="Ingrese mes"
 //        in3.hint="Ingrese año"
-//        in1.isVisible= false
+        in1.isVisible= false
         in2.isVisible= false
         in3.isVisible= false
         in4.isVisible= false
         in5.isVisible= false
         button.setOnClickListener {
-            val num1 = in1.text.toString().toInt()
+//            val num1 = in1.text.toString().toInt()
 //            val num2 = in2.text.toString().toInt()
 //            val num3 = in3.text.toString().toInt()
 //            val num4 = in4.text.toString().toInt()
@@ -35,27 +35,11 @@ class MainActivity : AppCompatActivity() {
             //            val division = num1 / num2
             //          val cuadrado=num1*4
             //            val promedio = suma / 3
-            var x =0
-            do {
-                //esto crea un bucle infinito y peta, no es traducible parece
-                x = num1
-                if (x>-1)
-                    if (x <10)
-                        resultado.text =("el numero $x tiene 1 cifra")
-                    else if (x <100)
-                        resultado.text =("el numero $x tiene 2 cifras")
-                    else if (x<1000)
-                        resultado.text =("el numero $x tiene 3 cifras")
-                    else
-                        resultado.text = "error"
-                else
-                    resultado.text = "negativo no validos"
-            }while (x !=0)
-
-
-
-
-}
+            var palabras =""
+            for(i in 1..100)
+                palabras+=("$i\n")
+            resultado.text =palabras
+        }
     }
 
 
@@ -65,12 +49,30 @@ class MainActivity : AppCompatActivity() {
 
 
 
-
-
-fun main(parametro: Array<String>) {
-    var mult8 = 8
-    while (mult8 <= 500) {
-        print("$mult8 -")
-        mult8 = mult8 + 8
-    }
-}
+//var suma = 0
+//
+//var x=0
+//
+//do {
+//
+//
+//    if(suma!=9999){
+//        suma += arrayListo[x]
+//
+//    }
+//    x++
+//} while(x<4 && arrayListo[x]!=9999)
+//if (suma==0)
+//resultado.text =("El valor acumulado es:  $suma \n")+
+//("El valor acumulado es cero.")
+//else
+//if (suma>0)
+//resultado.text =("El valor acumulado es:  $suma \n")+
+//("El valor acumulado es positivo.")
+//else
+//resultado.text =("El valor acumulado es:  $suma \n")+
+//("El valor acumulado es negativo")
+//
+//
+//
+//
