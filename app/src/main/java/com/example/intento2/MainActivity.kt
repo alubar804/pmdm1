a1362b37e7dfa8e27417bb0b9b1a3966e5301951
alu10216804 <alubar804@ieselcaminas.org>
2021-10-07 20:30:30 +0200

    hasta el 73

--- app/src/main/java/com/example/intento2/MainActivity.kt
+++ app/src/main/java/com/example/intento2/MainActivity.kt
@@ -14,43 +14,59 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        enunciado.text = "Proyecto 69"
+        enunciado.text = "Proyecto 73"
 //        in1.isVisible= false
 //        in2.isVisible= false
 //        in3.isVisible= false
-//        in4.isVisible= false
+        in4.isVisible= false
         in5.isVisible= false
+        resultado.text =""
         button.setOnClickListener {
-            val num1 = in1.text.toString().toInt()
-            val num2 = in2.text.toString().toInt()
-            val num3 = in3.text.toString().toInt()
-            val num4 = in4.text.toString().toInt()
-//            //            val num5 = in5.text.toString().toInt()
-            val arrayListo =  arrayOf<Int>(num1,num2,num3,num4)
-            var palabras =""
-            var cont0 =0
-            var cont2 =0
-            var cont1 =0
-            var contMas=0
-            for (i in arrayListo)
-                when(i){
-                    0 -> cont0++
-                    1 -> cont1++
-                    2 -> cont2++
-                    else -> contMas++
 
-                }
-            palabras= "the families have this many children:\n $cont0 zero children \n $cont1 one child\n" +
-                    " $cont2 two children\n $contMas have more than two"
-            resultado.text =palabras
+            operacion1()
+            separador()
+            operacion1()
+
+
+
+
 
         }
     }
 
 
+    fun operacion1(){
+        var palabras =resultado.text.toString()
+        val num1 = in1.text.toString().toInt()
+        val num2 = in2.text.toString().toInt()
+        val num3 = in3.text.toString().toInt()
+//        val num4 = in4.text.toString().toInt()
+//            //            val num5 = in5.text.toString().toInt()
+//        val arrayListo =  arrayOf<Int>(num1,num2,num3,num4)
+
+        val menor = if (num1 < num2 && num1 < num3) num1 else if (num2 < num3) num2 else num3
+        palabras+="The smallest number of inputed ones is $menor\n"
+        resultado.text =palabras
+    }
+    fun operacion2(){
+        var palabras =resultado.text.toString()
+//        val num1 = in1.text.toString().toInt()
+        val num2 = in2.text.toString().toInt()
+        val num3 = in3.text.toString().toInt()
+//        val num4 = in4.text.toString().toInt()
+//            //            val num5 = in5.text.toString().toInt()
+//        val arrayListo =  arrayOf<Int>(num1,num2,num3,num4)
+        val suma = num3+num2
+        palabras+="The value of the sum of $num2 and $num3 is $suma\n"
+        resultado.text =palabras
+    }
+    fun separador(){
+        var palabras =resultado.text.toString()
+        palabras+=("*******************************\n")
+
+        resultado.text =palabras
+    }
+
 }
-
-
-
 
 
