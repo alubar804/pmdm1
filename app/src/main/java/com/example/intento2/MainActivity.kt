0d57b834bd07bc37c2e1e848cedf88420f14dce0
alu10216804 <alubar804@ieselcaminas.org>
2021-10-13 18:35:59 +0200

    hasta 80

--- app/src/main/java/com/example/intento2/MainActivity.kt
+++ app/src/main/java/com/example/intento2/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import androidx.core.view.isVisible
 import com.example.intento2.*
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,18 +15,25 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        enunciado.text = "Proyecto 73"
+        enunciado.text = "Proyecto 80"
 //        in1.isVisible= false
 //        in2.isVisible= false
-//        in3.isVisible= false
+        in3.isVisible= false
         in4.isVisible= false
         in5.isVisible= false
         resultado.text =""
-        button.setOnClickListener {
 
-            operacion1()
-            separador()
-            operacion1()
+
+        button.setOnClickListener {
+            val num1 = in1.text.toString().toInt()
+            val num2 = in2.text.toString().toInt()
+//            val num3 = in3.text.toString().toInt()
+//        val num4 = in4.text.toString().toInt()
+//            //            val num5 = in5.text.toString().toInt()
+            mensaje("This program returns the biggest out of two numbers")
+            var valor = operacion(num1,num2)
+            mensaje("The biggest number is $valor")
+            mensaje("End of the program, thanks for using it")
 
 
 
@@ -35,37 +43,25 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    fun operacion1(){
-        var palabras =resultado.text.toString()
-        val num1 = in1.text.toString().toInt()
-        val num2 = in2.text.toString().toInt()
-        val num3 = in3.text.toString().toInt()
-//        val num4 = in4.text.toString().toInt()
-//            //            val num5 = in5.text.toString().toInt()
-//        val arrayListo =  arrayOf<Int>(num1,num2,num3,num4)
 
-        val menor = if (num1 < num2 && num1 < num3) num1 else if (num2 < num3) num2 else num3
-        palabras+="The smallest number of inputed ones is $menor\n"
-        resultado.text =palabras
+    fun operacion(num1:Int,num2:Int) : Int{
+        if (num1>num2){
+            return num1
+        }else{
+            return num2
+        }
+
+
     }
-    fun operacion2(){
-        var palabras =resultado.text.toString()
-//        val num1 = in1.text.toString().toInt()
-        val num2 = in2.text.toString().toInt()
-        val num3 = in3.text.toString().toInt()
-//        val num4 = in4.text.toString().toInt()
-//            //            val num5 = in5.text.toString().toInt()
-//        val arrayListo =  arrayOf<Int>(num1,num2,num3,num4)
-        val suma = num3+num2
-        palabras+="The value of the sum of $num2 and $num3 is $suma\n"
-        resultado.text =palabras
-    }
-    fun separador(){
+
+    fun mensaje(mensaje:String){
         var palabras =resultado.text.toString()
         palabras+=("*******************************\n")
-
+        palabras+= "$mensaje \n"
+        palabras+=("*******************************\n")
         resultado.text =palabras
     }
+
 
 }
 
