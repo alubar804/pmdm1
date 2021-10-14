ce75f9e43332c6d78ddd6e271ec8b43122d5b820
alu10216804 <alubar804@ieselcaminas.org>
2021-10-14 20:17:43 +0200

    hasta 93

--- app/src/main/java/com/example/intento2/MainActivity.kt
+++ app/src/main/java/com/example/intento2/MainActivity.kt
@@ -15,41 +15,37 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        enunciado.text = "Proyecto 83"
+        enunciado.text = "Proyecto 93"
 //        in1.isVisible= false
-        in2.isVisible= false
-        in3.isVisible= false
-        in4.isVisible= false
-        in5.isVisible= false
+//        in2.isVisible= false
+//        in3.isVisible= false
+//        in4.isVisible= false
+//        in5.isVisible= false
         resultado.text =""
 
 
         button.setOnClickListener {
-            val num1 = in1.text.toString().toInt()
-//            val num2 = in2.text.toString().toInt()
-//                        val num3 = in3.text.toString().toInt()
-            //        val num4 = in4.text.toString().toInt()
-            //            //            val num5 = in5.text.toString().toInt()
-            mensaje("This program returns the average of tree numbers")
-
-            mensaje("the perimeter equals ${operacion(num1)} ")
+            val num1 = in1?.text.toString().toInt()
+            val num2 = in2?.text.toString().toInt()
+            val num3 = in3?.text.toString().toInt()
+            val num4 = in4?.text.toString().toInt()
+            val num5 = in5?.text.toString().toInt()
+            mensaje("This program adds a subtitle mad of a specified string")
+            operacion(num1)
+            operacion(num1,num2)
+            operacion(num1,num2,num3)
+            operacion(num1,num2,num3,num4)
+            operacion(num1,num2,num3,num4,num5)
             mensaje("End of the program, thanks for using it")
-
         }
     }
 
-
-
-
-
-        fun operacion(num1:Int) : Int{
-            val pro = (num1)*4
-            return pro
-
-
-        }
-
-
+    fun operacion(num1:Int,num2:Int=0,num3:Int=0,num4:Int=0,num5:Int=0,) {
+        var palabras =resultado.text.toString()
+        val op= num1+num2+num3+num4+num5
+        palabras+="$op\n"
+        resultado.text =palabras
+    }
 
 
     fun mensaje(mensaje:String){
@@ -59,7 +55,6 @@ class MainActivity : AppCompatActivity() {
         palabras+=("*******************************\n")
         resultado.text =palabras
     }
-
 
 }
 
