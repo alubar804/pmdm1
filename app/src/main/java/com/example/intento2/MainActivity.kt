a879a23a34d89e9608c3f003198e1f4d0675756e
alu10216804 <alubar804@ieselcaminas.org>
2021-10-18 18:55:52 +0200

    hasta 101

--- app/src/main/java/com/example/intento2/MainActivity.kt
+++ app/src/main/java/com/example/intento2/MainActivity.kt
@@ -1,61 +1,76 @@
 package com.example.intento2
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
 import androidx.core.view.isVisible
-import com.example.intento2.*
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.math.pow
 
 class MainActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        enunciado.text = "Proyecto 93"
-//        in1.isVisible= false
-//        in2.isVisible= false
-//        in3.isVisible= false
-//        in4.isVisible= false
-//        in5.isVisible= false
-        resultado.text =""
-
+        enunciado.text = "Proyecto 101"
+        in1.isVisible = false
+        in2.isVisible = false
+        in3.isVisible = false
+        in4.isVisible = false
+        in5.isVisible = false
+        resultado.text = ""
 
         button.setOnClickListener {
-            val num1 = in1?.text.toString().toInt()
-            val num2 = in2?.text.toString().toInt()
-            val num3 = in3?.text.toString().toInt()
-            val num4 = in4?.text.toString().toInt()
-            val num5 = in5?.text.toString().toInt()
-            mensaje("This program adds a subtitle mad of a specified string")
-            operacion(num1)
-            operacion(num1,num2)
-            operacion(num1,num2,num3)
-            operacion(num1,num2,num3,num4)
-            operacion(num1,num2,num3,num4,num5)
-            mensaje("End of the program, thanks for using it")
+//            val num1 = in1.text.toString().toFloat()
+//            val num2 = in2.text.toString().toFloat()
+//            val num3 = in3.text.toString().toFloat()
+//            val num4 = in4.text.toString().toFloat()
+//            val num5 = in5.text.toString().toFloat()
+            mensajeComplex("This program returns the first and last elements of an array")
+//            val arrayNumeros:FloatArray = FloatArray(5)
+//            arrayNumeros[0]=num1
+//            arrayNumeros[1]=num2
+//            arrayNumeros[2]=num3
+//            arrayNumeros[3]=num4
+//            arrayNumeros[4]=num5
+            val arrayNumeros= arrayOf<Int>(1,2,3,4,5,6,7,8,9,10)
+            mensajeArray(arrayNumeros)
         }
     }
+    fun mensajeArray(elarray: Array<Int>) {
+        var palabras = resultado.text.toString()
 
-    fun operacion(num1:Int,num2:Int=0,num3:Int=0,num4:Int=0,num5:Int=0,) {
-        var palabras =resultado.text.toString()
-        val op= num1+num2+num3+num4+num5
-        palabras+="$op\n"
-        resultado.text =palabras
+            palabras += "the first number is ${elarray[0]} \n"
+
+            palabras += "the last number is ${elarray.last()}\n"
+
+
+        resultado.text = palabras
+    }
+    fun operacion() {
+        var palabras = resultado.text.toString()
+        fun subOperacion(pr: Int, sg: Int) = if (pr > sg) pr else sg
+        val array = arrayListOf<Int>(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+
+        for (i in array) {
+            subOperacion(i, 5)
+            palabras += "out of $i and 5, ${subOperacion(i, 5)} is bigger\n"
+
+            resultado.text = palabras
+        }
+    }
+    fun mensaje(mensaje: String) {
+        var palabras = resultado.text.toString()
+        palabras += "$mensaje \n"
+        resultado.text = palabras
+    }
+    fun mensajeComplex(mensaje: String) {
+        var palabras = resultado.text.toString()
+        palabras += ("*******************************\n")
+        palabras += "$mensaje \n"
+        palabras += ("*******************************\n")
+        resultado.text = palabras
     }
 
-
-    fun mensaje(mensaje:String){
-        var palabras =resultado.text.toString()
-        palabras+=("*******************************\n")
-        palabras+= "$mensaje \n"
-        palabras+=("*******************************\n")
-        resultado.text =palabras
     }
-
-}
 
 
